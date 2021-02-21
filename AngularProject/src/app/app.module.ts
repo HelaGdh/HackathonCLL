80da89fd958bff9dc227e1e223d7d13ad13d8f17
Lanygrebs <yassine.melliti@etudiant-fst.utm.tn>
2021-02-21 17:24:56 +0100

    navbar

--- AngularProject/src/app/app.module.ts
+++ AngularProject/src/app/app.module.ts
@@ -2,10 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NvbarComponent } from './nvbar/nvbar.component';
+
+import { IgxNavbarModule } from 'igniteui-angular';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { RegisterComponent } from './components/register/register.component'
+
+/* Angular material */
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@NgModule({
+  declarations: [...],
+  imports: [
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+  ],
+  providers: [...],
+  bootstrap: [...],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+
+export class AppModule { }
+
+@NgModule({
+  ...
+  imports: [..., IgxNavbarModule],
+  ...,
+  declarations: [LogInComponent, RegisterComponent]
+})
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NvbarComponent
   ],
   imports: [
     BrowserModule
